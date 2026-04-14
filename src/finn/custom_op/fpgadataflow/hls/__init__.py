dce2e62cd0362542279becd50da45cde87af1adc
Felix Jentzsch <felix.jentzsch@upb.de>
2026-04-14 15:38:11 +0200

    Fix import

--- src/finn/custom_op/fpgadataflow/hls/__init__.py
+++ src/finn/custom_op/fpgadataflow/hls/__init__.py
@@ -26,6 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from finn.custom_op.fpgadataflow.hls.streamingfifo_hls import StreamingFIFO_hls
 from finn.custom_op.fpgadataflow.hlsbackend import HLSBackend
 from finn.custom_op.fpgadataflow.hwcustomop import HWCustomOp
 
