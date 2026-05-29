390a27ce0a629f723caf153f4dddb2d64f2d73b0
Felix Jentzsch <felix.jentzsch@upb.de>
2026-05-29 12:26:00 +0200

    Fix driver configs, dependencies

--- scripts/driver_install_dependencies.sh
+++ scripts/driver_install_dependencies.sh
@@ -5,4 +5,9 @@ sudo pip install finn-dataset-loading
 sudo pip install onnx==1.17.0
 sudo pip install qonnx==1.0.0
 sudo pip install h5py
-sudo pip install pynqmetadata==0.1.5 # Workaround for https://discuss.pynq.io/t/how-to-address-axilite-interface-in-pynq-v3-0/4831
+# Ensure other installs don't upgrade numpy too far:
+sudo pip install "numpy<2.0.0"
+# Somehow this was missing after above installs:
+sudo pip install grpcio==1.64.0
+# Required for boards with older PYNQ images (< 3.1.1):
+#sudo pip install pynqmetadata==0.1.5 # Workaround for https://discuss.pynq.io/t/how-to-address-axilite-interface-in-pynq-v3-0/4831
