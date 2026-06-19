6e7f30d9bab5c441279d3400cd83fcce2d5cf489
Thomas B. Preußer <thomas.preusser@amd.com>
2026-06-19 13:48:40 +0200

    Linting.

--- finn-rtllib/sim/hdl/sim_ctrl.v
+++ finn-rtllib/sim/hdl/sim_ctrl.v
@@ -2,8 +2,8 @@ module sim_ctrl(input ap_clk, input sim_finish, output sim_ctrl_out);
 	assign sim_ctrl_out = 1'b0;
 `ifdef FINN_SIMULATION
 	always @(posedge sim_finish) $finish;
-	// This ensures there is always a pending #delay in the event queue, 
-        // preventing the kernel from concluding that the simulation is ending.
+	// This ensures there is always a pending #delay in the event queue,
+	// preventing the kernel from concluding that the simulation is ending.
 	initial forever #1_000_000_000;
 `endif
 endmodule
