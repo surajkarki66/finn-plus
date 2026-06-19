7e000762aceb901d9cf85291fc3c159534c614c3
Shane Fleming <sfleming@xilinx.com>
2026-06-19 11:45:02 +0100

    [FIFO Debug] Increase busy loop delay to keep sim alive

--- finn-rtllib/sim/hdl/sim_ctrl.v
+++ finn-rtllib/sim/hdl/sim_ctrl.v
@@ -2,9 +2,8 @@ module sim_ctrl(input ap_clk, input sim_finish, output sim_ctrl_out);
 	assign sim_ctrl_out = 1'b0;
 `ifdef FINN_SIMULATION
 	always @(posedge sim_finish) $finish;
-	// Workaround for XSI bug: final blocks execute prematurely when all
-	// initial blocks complete, rather than at $finish. This never-completing
-	// initial block prevents that.
-	initial forever #1;
+	// This ensures there is always a pending #delay in the event queue, 
+        // preventing the kernel from concluding that the simulation is ending.
+	initial forever #1_000_000_000;
 `endif
 endmodule
