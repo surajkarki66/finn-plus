2c1e50e650cbccc3fe48db0bd6638552e8241b2e
Thomas B. Preußer <thomas.preusser@xilinx.com>
2026-06-19 13:41:06 +0100

    Fixing test.

--- finn-rtllib/sim/hdl/sim_ctrl.v
+++ finn-rtllib/sim/hdl/sim_ctrl.v
@@ -1,6 +1,13 @@
+/****************************************************************************
+ * Copyright Advanced Micro Devices, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @brief	Simulation control triggering $finish upon asserting sim_finish.
+ * @author	Shane T. Fleming <shane.fleming@amd.com>
+ ***************************************************************************/
 module sim_ctrl(input ap_clk, input sim_finish);
 `ifdef FINN_SIMULATION
-	always @(posedge sim_finish) $finish;
+	initial @(posedge sim_finish) $finish;
 	// This ensures there is always a pending #delay in the event queue,
 	// preventing the kernel from concluding that the simulation is ending.
 	initial forever #1_000_000_000;
