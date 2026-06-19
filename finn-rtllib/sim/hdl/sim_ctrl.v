44fc768d925b24beb00b2f2fe7106f5ad8403e71
Thomas B. Preußer <thomas.preusser@xilinx.com>
2026-06-19 13:00:27 +0100

    Prune unused module port.

--- finn-rtllib/sim/hdl/sim_ctrl.v
+++ finn-rtllib/sim/hdl/sim_ctrl.v
@@ -1,5 +1,4 @@
-module sim_ctrl(input ap_clk, input sim_finish, output sim_ctrl_out);
-	assign sim_ctrl_out = 1'b0;
+module sim_ctrl(input ap_clk, input sim_finish);
 `ifdef FINN_SIMULATION
 	always @(posedge sim_finish) $finish;
 	// This ensures there is always a pending #delay in the event queue,
