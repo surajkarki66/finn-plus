6d6963bfacd340b08dec1252324eeac1697bf6ec
bwintermann <bjarne.wintermann@uni-paderborn.de>
2026-07-08 16:46:39 +0200

    Simplified blocking static mux, work in progress

--- custom_hls/mux/static_schedule/static_mux.hpp
+++ custom_hls/mux/static_schedule/static_mux.hpp
@@ -1,5 +1,5 @@
-#ifndef STATIC_MUX_H
-#define STATIC_MUX_H
+#ifndef STATIC_MUX_HPP
+#define STATIC_MUX_HPP
 
 #include "ap_int.h"
 #include "hls_stream.h"
@@ -9,25 +9,31 @@
 #include <functional>
 #include <utility>
 
+constexpr std::size_t bitwidth(std::size_t i) {
+    // Every bit halves the number of choices addressed, so the
+    // next call only deals with i/2 (i>>1)
+    return (i < 2 ? i : 1 + bitwidth(i >> 1));
+}
+static_assert(bitwidth(2) == 2, "Bitwidth for 2 should be 2");
+static_assert(bitwidth(11) == 4, "Bitwidth for 11 should be 4");
+
+
 inline void _static_mux() {}
 
-template<typename TOut, int FOut, typename T, int F>
+template<std::size_t idx = 0, typename TOut, int FOut, typename T, int F>
 void _static_mux(hls::stream<TOut, FOut>& out, std::reference_wrapper<hls::stream<T, F>> &stream) {
-    if (!stream.get().empty()) {
-        out.write(stream.get().read());
-    }
+    if (!stream.get().empty()) { out.write(stream.get().read()); }
 }
 
-template<typename TOut, int FOut, typename T, int F, typename ...Ts, int ...Fs>
+template<std::size_t idx = 0, typename TOut, int FOut, typename T, int F, typename ...Ts, int ...Fs>
 void _static_mux(hls::stream<TOut, FOut>& out, std::reference_wrapper<hls::stream<T, F>> &stream, std::reference_wrapper<hls::stream<Ts, Fs>>& ...others) {
-    if (!stream.get().empty()) {
-        out.write(stream.get().read());
-    }
-    _static_mux(out, others...);
+    // TODO: ADD HEADER
+    if (!stream.get().empty()) { out.write(stream.get().read()); }
+    _static_mux<idx+1>(out, others...);
 }
 
 template<
-    typename TOut,
+    int N,
     int FOut,
     typename ...Ts,
     int ...Fs,
@@ -35,7 +41,7 @@ template<
     >
 void static_mux(
     std::index_sequence<I...>,
-    hls::stream<TOut, FOut> &out,
+    hls::stream<ap_uint<N>, FOut> &out,
     hls::stream<Ts, Fs>& ...others
 ) {
     std::tuple<std::reference_wrapper<hls::stream<Ts, Fs>>...> streams = std::make_tuple(std::ref(others)... );
